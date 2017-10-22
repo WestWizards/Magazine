31ab86bd5f3378ded9b77fd57e1340ccf21f5114
Quantum179 <steeven.tell@gmail.com>
2017-10-21 23:49:39 -0400

    forgot some files in init client

--- api/app.py
+++ api/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from route import Route
 
-
-
 app = Flask(__name__)
+CORS(app)
 router = Route(app)
 
 # use Deployment Options instead of run for prod
--- api/route.py
+++ api/route.py
@@ -1,6 +1,7 @@
 from bson.json_util import loads, dumps
 from pymongo import MongoClient
 
+# TODO : réduire les redondances de code
 
 class Route:
     def __init__(self, app):
@@ -13,13 +14,36 @@ class Route:
 
         @app.route("/magazine")
         def get_magazine():
+            # TODO : Discuter des données contenues dans l'accueil du magazine
+
             return "Magazine"
 
         @app.route("/users/")
         def get_users():
             print("debug get_users")
             result_query = dumps(db.users.find())
-
             res = app.response_class(response=result_query, status=200,headers=None, mimetype="json", direct_passthrough=False)
+
+            return res
+
+        @app.route("/posts/", methods=['GET'])
+        def get_posts():
+            print("debut get posts")
+            result_query = dumps(db.posts.find())
+            res = app.response_class(response=result_query, status=200,headers=None, mimetype="json", direct_passthrough=False)
+
+            return res
+
+        @app.route("/posts/", methods=['POST'])
+        def add_post():
+            print("debug add post")
+
+            return 'post added'
+
+        @app.route("/posts/<int:uuidPost>")
+        def get_post(uuidPost):
+            print("debug get post")
+            result_query = dumps(db.posts.find(uuidPost))
+            res = app.response_class(response=result_query, status=200, headers=None, mimetype="json", direct_passthrough=False)
 
             return res
